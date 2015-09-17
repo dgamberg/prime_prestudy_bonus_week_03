beb7c3cf2e61d4d048e0196afc3d215731d4662c
Dave Gamberg <dgamberg@gmail.com>
2015-09-16 22:03:20 -0500

    addingthe4 functions and call them in one chain

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hey this works!");
+//Write 4 functions and call them all in one long chain
+
+// Function 1
+function cubeIt(a){
+	return a * a * a;
+}
+
+// Function2
+function subtractTwoHundred(b){
+	return b - 100;
+}
+
+// Function 3
+function cutItInHalf(c){
+	return c / 2;
+}
+
+// Function 4
+function multiplyTimesSix(d){
+	return d * 6;
+}
+
+// Call all 4 functions in one chain
+var output = multiplyTimesSix(cutItInHalf(subtractTwoHundred(cubeIt(6))));
+
+//output result
+console.log(output);
